99014c969d716b39d5cbdf02199359e256441425
Kasper Nyhus <kanyhus@gmail.com>
2021-03-04 10:05:42 +0100

    registrer

--- KAudlejning/settings.py
+++ KAudlejning/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registrations.apps.RegistrationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
