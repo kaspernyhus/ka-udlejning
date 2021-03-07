06e4a1b0f358bf6b3d432545352c60f2b8f23716
Kasper Nyhus <kanyhus@gmail.com>
2021-03-07 11:54:11 +0100

    gomore stats

--- statistik/views.py
+++ statistik/views.py
@@ -1,4 +1,4 @@
-from registrations.models import Tankning, Tur, Udgift
+from registrations.models import Indbetaling, Tankning, Tur, Udgift
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
@@ -8,19 +8,33 @@ def show_stats(request):
   first_entry = Tur.objects.first()
   latest_entry = Tur.objects.latest('id')
   total_km = latest_entry.odometer - first_entry.odometer
+  # GoMore stats
+  gomore_ture = Tur.objects.filter(user=6) # GoMore user id = 6
+  gomore_km = 0
+  for tur in gomore_ture:
+    try:
+      gomore_km += tur.delta_km
+    except:
+      pass
+  total_km_minus_gomore = total_km - gomore_km
+  gomore_indbetalinger = Indbetaling.objects.all().filter(user=6)
+  gomore_income = 0
+  for indbetaling in gomore_indbetalinger:
+     gomore_income += indbetaling.amount
+  gomore_income_pr_km = gomore_income / gomore_km
   # Benzin price pr. km
   tankninger = Tankning.objects.all()
   tankning_total = 0
   for tankning in tankninger:
     tankning_total += tankning.amount
-  gas_price_pr_km = tankning_total / total_km
+  gas_price_pr_km = tankning_total / total_km_minus_gomore
   # Udgifter
   udgifter = Udgift.objects.all()
   udgifter_total = 0
   for udgift in udgifter:
     udgifter_total += udgift.amount
   # Total price pr km
-  total_expenses = tankning_total + udgifter_total
+  total_expenses = tankning_total + udgifter_total - gomore_income
   price_pr_km = total_expenses / total_km
   # User usage
   users = User.objects.all()
@@ -36,5 +50,15 @@ def show_stats(request):
     user_usage = (user_km_total / total_km) * 100 
     user_usages.append({'user': user, 'user_km': user_km_total, 'usage': round(user_usage,2)})
   
-  context = {'total_km': total_km, 'benzinpris_pr_km': gas_price_pr_km, 'total_expenses': udgifter_total, 'pris_pr_km': price_pr_km, 'user_usage': user_usages[1:]}
+  
+  context = {
+    'total_km': total_km,
+    'benzinpris_pr_km': gas_price_pr_km, 
+    'total_expenses': udgifter_total, 
+    'pris_pr_km': price_pr_km, 
+    'user_usage': user_usages[1:],
+    'gomore_km': gomore_km,
+    'gomore_income': gomore_income,
+    'gomore_income_pr_km': gomore_income_pr_km
+    }
   return render(request, 'statistik/stats.html', context)
